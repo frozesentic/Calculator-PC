e796934d8fe079854b19127fc16d8f951ce2e4cb
Git-Support / XCC2 <99868523+frozesentic@users.noreply.github.com>
2023-08-06 22:25:59 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+// service-worker.js
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('app-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/main.js',
+          '/styles.css',
+          /* Add more assets to cache */
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
